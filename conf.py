877937a6c392bbe246e16076d2372f2e9c1b6a5c
Ivan Zarubin <ivanzar.developer@gmial.com>
2019-03-23 21:36:57 +0300

    updt

--- conf.py
+++ conf.py
@@ -92,7 +92,7 @@ html_theme = "sphinx_rtd_theme"
 # html_theme_options = {}
 
 html_logo = "./img/logo.png"
-html_favicon ="icon.png"
+html_favicon ="./img/icon.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
